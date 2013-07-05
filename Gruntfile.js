05f91902e5cd6a3f4e6f0d2f0e4637143138822a
Josh Gillham <usajoshgillham@gmail.com>
2013-07-05 16:57:05 -0600

    Not using grunt-mocha.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,9 @@
 module.exports = function( grunt ) {
-    /** Grunt plugins. **/
-    grunt.loadNpmTasks( "grunt-mocha" );
-    // TODO grunt.loadNpmTasks for each grunt plugin.
-    
-    /** Tasks. **/
-    grunt.registerTask( "check", [ "mocha" ] );
-    grunt.registerTask( "test", [ "check" ] );
+	// TODO grunt.loadNpmTasks for each grunt plugin.
 	// TODO grunt.registerTask for each target.
-    
-    /** Task options. **/
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' )
-        ,
-        mocha: {
-            all: [ "test/*Test.js" ]
-        }
-		// TODO task options for each task.
+		// TODO task data for each task.
 	} )
+    grunt.registerTask( "test", [ "TODO" ] );
 }
