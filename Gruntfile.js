d9e16d23750345054a3ff0b78f96730289d53bb1
Josh Gillham <usajoshgillham@gmail.com>
2013-07-05 16:34:54 -0600

    Closes #3. Installed mocha testing framework.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,20 @@
 module.exports = function( grunt ) {
-	// TODO grunt.loadNpmTasks for each grunt plugin.
+    /** Grunt plugins. **/
+    grunt.loadNpmTasks( "grunt-mocha" );
+    // TODO grunt.loadNpmTasks for each grunt plugin.
+    
+    /** Tasks. **/
+    grunt.registerTask( "check", [ "mocha" ] );
+    grunt.registerTask( "test", [ "check" ] );
 	// TODO grunt.registerTask for each target.
+    
+    /** Task options. **/
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' )
-		// TODO task data for each task.
+        ,
+        mocha: {
+            all: [ "test/*Test.js" ]
+        }
+		// TODO task options for each task.
 	} )
-    grunt.registerTask( "test", [ "TODO" ] );
 }
