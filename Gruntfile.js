270895708a666f4c29ad8d28ed83587bb544d23b
Josh Gillham <usajoshgillham@gmail.com>
2013-07-05 15:09:56 -0600

    Closes #2. Installed grunt automated task manager.

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function( grunt ) {
+	// TODO grunt.loadNpmTasks for each grunt plugin.
+	// TODO grunt.registerTask for each target.
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' )
+		// TODO task data for each task.
+	} )
+    grunt.registerTask( "test", [ "TODO" ] );
+}
